8ce5819fbf8a96676d6811be1b02526edb92712f
Stefan Jucker <stefan.jucker@gmail.com>
2025-08-09 20:52:23 +0200

    Revert "Heroku debugging"
    This reverts commit 8fc97a6a2fab994d914a355ca816950e1690c272.

--- src/main/java/ch/refereecoach/probasket/security/JwtTokenService.java
+++ src/main/java/ch/refereecoach/probasket/security/JwtTokenService.java
@@ -3,7 +3,6 @@ package ch.refereecoach.probasket.security;
 import ch.refereecoach.probasket.configuration.ApplicationProperties;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +11,6 @@ import javax.crypto.spec.SecretKeySpec;
 import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-@Slf4j
 @Component
 public class JwtTokenService {
 
@@ -21,7 +19,6 @@ public class JwtTokenService {
     private final SecretKeySpec secretKey;
 
     public JwtTokenService(ApplicationProperties properties) {
-        log.info(properties.toString());
         this.secretKey = new SecretKeySpec(properties.getJwtSecret().getBytes(UTF_8), SIGNATURE_ALGORITHM.getJcaName());
     }
 
