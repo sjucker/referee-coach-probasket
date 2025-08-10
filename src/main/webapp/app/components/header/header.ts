d8df1aae562edb76bf268f4cd2fe9868c3b62397
Stefan Jucker <stefan.jucker@gmail.com>
2025-08-10 11:26:13 +0200

    Update header to include navigation menu and enhance accessibility

--- src/main/webapp/app/components/header/header.ts
+++ src/main/webapp/app/components/header/header.ts
@@ -3,11 +3,12 @@ import {MatToolbar, MatToolbarRow} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {AuthService} from "../../auth.service";
 import {NavigationEnd, Router, RouterLink} from "@angular/router";
-import {PATH_LOGIN, PATH_OVERVIEW} from "../../app.routes";
+import {PATH_LOGIN, PATH_OVERVIEW, PATH_TAG_SEARCH} from "../../app.routes";
 import {MatTooltip} from "@angular/material/tooltip";
 import {MatIcon} from "@angular/material/icon";
 import {toSignal} from '@angular/core/rxjs-interop';
 import {filter, map, startWith} from 'rxjs';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
     selector: 'app-header',
@@ -18,6 +19,7 @@ import {filter, map, startWith} from 'rxjs';
         MatTooltip,
         MatIcon,
         RouterLink,
+        MatMenuModule,
     ],
     templateUrl: './header.html',
     styleUrl: './header.scss',
@@ -25,6 +27,7 @@ import {filter, map, startWith} from 'rxjs';
 })
 export class Header {
     protected readonly PATH_OVERVIEW = PATH_OVERVIEW;
+    protected readonly PATH_TAG_SEARCH = PATH_TAG_SEARCH;
     protected readonly auth = inject(AuthService);
     protected readonly isOverview = computed(() => this.currentUrl().startsWith('/' + PATH_OVERVIEW));
 
