d97d965a62271451f824bb267124985473c6b734
Stefan Jucker <jucker@mptechnology.ch>
2025-08-10 20:41:58 +0200

    Remove unused SecuredEndpoint and add CrossOrigin support to REST endpoints

--- src/main/java/ch/refereecoach/probasket/rest/BasketplanEndpoint.java
+++ src/main/java/ch/refereecoach/probasket/rest/BasketplanEndpoint.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.time.LocalDate;
 import static ch.refereecoach.probasket.common.OfficiatingMode.OFFICIATING_2PO;
 
 @Slf4j
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/basketplan")
 @RequiredArgsConstructor
--- src/main/java/ch/refereecoach/probasket/rest/ReportEndpoint.java
+++ src/main/java/ch/refereecoach/probasket/rest/ReportEndpoint.java
@@ -10,12 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/report")
 @RequiredArgsConstructor
