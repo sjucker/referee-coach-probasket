fdaba56c8418745cf5a51fa184a36d0f20cb0c6d
Stefan Jucker <stefan.jucker@gmail.com>
2025-08-30 15:16:27 +0200

    Display app version

--- src/main/webapp/app/app.ts
+++ src/main/webapp/app/app.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {version} from '../../../../package.json'
 
 @Component({
     selector: 'app-root',
@@ -8,4 +9,10 @@ import {RouterOutlet} from '@angular/router';
     styleUrl: './app.scss'
 })
 export class App {
+
+    version = ""
+
+    constructor() {
+        this.version = version
+    }
 }
