4ee362b5a6ca7456396ccebb5d80b41b732d1116
Stefan Jucker <stefan.jucker@gmail.com>
2025-09-18 20:58:21 +0200

    Add FIBA documentation links

--- src/main/webapp/app/components/game-info/game-info.ts
+++ src/main/webapp/app/components/game-info/game-info.ts
@@ -3,6 +3,9 @@ import {DatePipe, NgClass} from "@angular/common";
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {OfficiatingMode, RefereeReportDTO} from "../../../rest";
 import {ScoreUtil} from "../../util/score-util";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
     selector: 'app-game-info',
@@ -12,7 +15,10 @@ import {ScoreUtil} from "../../util/score-util";
         MatCardContent,
         MatCardHeader,
         MatCardTitle,
-        NgClass
+        NgClass,
+        MatMenuModule,
+        MatButtonModule,
+        MatIconModule,
     ],
     templateUrl: './game-info.html',
     styleUrl: './game-info.scss'
