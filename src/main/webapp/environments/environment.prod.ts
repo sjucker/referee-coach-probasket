dc04214d13b4c7b85a215d08c28689d95475a1e0
Stefan Jucker <stefan.jucker@gmail.com>
2026-09-21 16:17:40 +0200

    Switch to Ticino Basket base-URL

--- src/main/webapp/environments/environment.prod.ts
+++ src/main/webapp/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: "https://referee-coach-probasket-dde14a8fd2b3.herokuapp.com",
+    baseUrl: "https://ticino-basket-bdcc1302bc72.herokuapp.com",
 }
